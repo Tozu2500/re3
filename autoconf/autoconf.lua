e080fed4ac3b8f4397ccec8f7f9777f140fe242d
Tozu2500 <tomi.louhiniitty@gmail.com>
2024-10-22 01:17:42 +0300

    df

--- autoconf/autoconf.lua
+++ autoconf/autoconf.lua
@@ -16,3 +16,12 @@
 	end
 
 	return p.modules.autoconf
+
+	local p = premake
+
+	if not premake.modules.autoconf then
+		p.modules.autoconf = {}
+		p.modules.autoconf._VERSION = p._VERSION
+		verbosef('Loading autoconf module...')
+		include('api.lua')
+		include('msc.lua')
